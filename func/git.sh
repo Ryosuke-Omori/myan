bdd51f70a8e18d2c4004571eb1e2c91d029bcf3c
r-omori <r-omori@commercelink.co.jp>
2018-05-15 16:40:23 +0900

    update git

--- func/git.sh
+++ func/git.sh
@@ -15,6 +15,23 @@ $ git reset --soft HEAD^    # コミットのみ
 
 # addしたあとのdiff
 $ git diff --cached [file]
+
+# 変更の一時的退避 stash
+# 変更の保存(saveは省略可)
+$ git stash save
+# 退避している変更の確認(-pで詳細も見れる)
+$ git stash list -p
+# 変更内容に含まれるファイル一覧の表示
+$ git stash show stash@{スタッシュ番号}
+# スタッシュの復活
+$ git stash apply stash@{スタッシュ番号}
+# スタッシュの削除
+$ git stash drop stash@{スタッシュ番号}
+# スタッシュの復活と削除
+$ git stash pop stash@{スタッシュ番号}
+# applyで復活させた変更を「やっぱやめとこ」ってなった時
+$ git stash show [適用したstash名] -p | git apply -R
+
 =========
 EOF
 }
